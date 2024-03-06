1fe248b0ee51f53e4377a9b1d050812589272387
Rafael M V <rafaelmendoncavaz@hotmail.com>
2024-03-06 17:30:03 -0300

    Adicionado comandos de exeecução para todas as 8 funções da barbearia

--- script.js
+++ script.js
@@ -19,27 +19,67 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for(let i = 0; i < barbearia.cortes.length; i++) {
+        if(barbearia.cortes[i].id === id) {
+            return barbearia.cortes[i]
+        }
+    }
+    return `Corte não encontrado`
 }
 
 function buscaBarbaPorId(id) {
+    for(let i = 0; i < barbearia.barbas.length; i++) {
+        if(barbearia.barbas[i].id === id) {
+            return barbearia.barbas[i]
+        }
+    }
+    return `Barba não encontrada`
 }
 
 function verificaStatusBarbearia() {
+    if(barbearia.estaAberto) {
+        return `Estamos abertos`
+    } else {
+        return `Estamos fechados`
+    }
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    let corteIs = buscaCortePorId(corteId)
+    let barbaIs = buscaBarbaPorId(barbaId)
+    let pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corteIs.tipo,
+        pedidoCortePreco: corteIs.valor,
+        pedidoBarba: barbaIs.tipo,
+        pedidoBarbaPreco: barbaIs.valor
+    }
+    return pedido
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    for(let i = 0; i < lista.length; i++) {
+        if(lista[i].id === id) {
+            lista[i].valor = valor
+            lista[i].tipo = tipo
+        }
+    }
+    return lista
 }
+// Se puder explicar para mim, o por quê que lista é lida diretamente como > barbearia.lista < (ou seja barbearia.cortes ou barbearia.barbas) eu agradeço. Pois tentei fazer do jeito que fazia sentido: Chamando a variável "barbearia" com a propriedade que deveria ser a lista mencionada no parâmetro
 
 function calculaTotal(pedido) {
+    let precoCorte = pedido.pedidoCortePreco
+    let precoBarba = pedido.pedidoBarbaPreco
+    let priceTotal = precoCorte + precoBarba
+
+    return priceTotal
 }
-  
-  
